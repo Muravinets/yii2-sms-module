d74a708af5f9c37ed626e1cc90c379d00b84de76
荒野無燈 <i@80x86.io>
2017-10-13 22:51:37 +0800

    sms: add alidayu vendor lib

---
+++ vendor/alidayu/lib/Core/IAcsClient.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace ihacklog\sms\vendor\alidayu\lib\Core;
+interface IAcsClient
+{
+	public function doAction($requst);
+}
---
+++ vendor/alidayu/lib/Core/Profile/IClientProfile.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ihacklog\sms\vendor\alidayu\lib\Core\Profile;
+
+interface IClientProfile
+{
+	public function getSigner();
+	
+	public function getRegionId();
+	
+	public function getFormat();
+	
+	public function getCredential();
+}
